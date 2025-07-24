ce660d8f9de963330e5eda07a8bf1307a205bf33
Muhammed. Suhail <mdsuhail63831@gmail.com>
2025-07-24 14:10:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,34 @@
+// User function Template for C++
+
+class Solution {
+  public:
+
+    int aggressiveCows(vector<int> &stalls, int k) {
+
+        // Write your code here
+        int n = stalls.size();
+         int start = 1, end, mid , ans;
+         sort(stalls.begin(),stalls.end());
+         end = stalls[n-1]- stalls[0];
+         
+         //binary search
+         while(start<=end){
+             mid = start+(end-start)/2;
+             int count = 1, pos = stalls[0];
+             for(int i =1;i<n;i++){
+                 if(pos+mid<=stalls[i]){
+                     count++;
+                     pos = stalls[i];
+                     }
+             }
+             if(count<k){
+                 end = mid-1;
+             }
+             else{
+                 ans = mid;
+                 start= mid+1;
+             }
+         }
+         return ans;
+    }
+};
